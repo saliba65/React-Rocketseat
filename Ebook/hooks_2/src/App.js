a6cf0eed442d098552cddeb8dd3d8a5e79c49ef3
saliba65 <lucassaliba10@gmail.com>
2020-09-10 16:47:52 -0300

    useReducer

--- Ebook/hooks_2/src/App.js
+++ Ebook/hooks_2/src/App.js
@@ -1,22 +1,28 @@
-import React, { createContext, useContext } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
-const ThemeContext = createContext();
-
 function App() {
+  const reducer = useReducer(
+    function (state, action) {},
+    "Lucas",
+    function (initialValue) {
+      return { name: initialValue };
+    }
+  );
+  console.log(reducer);
+
+  function handlePlus() {}
+
+  function handleMinus() {}
+
   return (
     <div className="App">
-      <ThemeContext.Provider value={{ mode: "dark" }}>
-        <Button />
-      </ThemeContext.Provider>
+      <h1>0</h1>
+      <h4>Cliques: 0</h4>
+      <button onClick={handlePlus}>+</button>
+      <button onClick={handleMinus}>-</button>
     </div>
   );
-}
-
-function Button() {
-  const theme = useContext(ThemeContext);
-
-  return <button>{theme.mode}</button>;
 }
 
 export default App;
